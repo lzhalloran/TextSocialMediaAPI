13d4ce877165e4020aee13e7d2cdd5fdbf1cb0cf
lzhalloran <lzhalloran@gmail.com>
2023-10-04 01:19:07 +1000

    validation for comments

--- src/schemas/comment_schema.py
+++ src/schemas/comment_schema.py
@@ -1,7 +1,10 @@
 from main import ma
 from marshmallow import fields
+from marshmallow.validate import Length
 
 class CommentSchema(ma.Schema):
+    text = fields.String(required=True, validate=Length(min=1, max=1000))
+
     class Meta:
         ordered = True
         fields = ("id", "text", "created_time", "user")
